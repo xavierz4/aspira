c027c4a0a24ccb749f07435ff9809b210b1d5872
javier <jjzoto@gmail.com>
2019-04-30 11:37:58 -0500

    recuperando

--- servidor/data/resolvers.js
+++ servidor/data/resolvers.js
@@ -16,7 +16,7 @@ export const resolvers = {
                     if(error) rejects(error)
                     else resolve(elector)
                 });
-            })
+            });
             
 
         },
